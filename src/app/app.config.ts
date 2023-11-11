b133df5c024e208668b6c5a18088476f905492e1
dmytroshapovalov <flami127@gmail.com>
2023-11-11 14:11:38 +0100

    firebase initialization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,5 +14,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimations(),
     importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
-    importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase()))]
+    importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"osbb-42912","appId":"1:666503050281:web:2bf8c108c5d44e3ef15838","databaseURL":"https://osbb-42912-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"osbb-42912.appspot.com","apiKey":"AIzaSyCKMey-uQt5aSF3bLfEg_mIYYczmq59dOA","authDomain":"osbb-42912.firebaseapp.com","messagingSenderId":"666503050281"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase()))]
 };
